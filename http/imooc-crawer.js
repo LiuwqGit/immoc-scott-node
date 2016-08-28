c8a1b51b9d3fa2dc00f0b480f7be1c2966fdc9d6
liuweiqin <772863869@qq.com>
2016-08-29 00:35:06 +0800

    获取慕课网的指定页面的章节和标题

--- http/imooc-crawer.js
+++ http/imooc-crawer.js
@@ -1,5 +1,68 @@
 var http = require('http');
+var cheerio = require('cheerio');
 var url = 'http://www.imooc.com/learn/348';
+
+function filterChapters(html) {
+	var $ = cheerio.load(html);
+	var chapters = $('.chapter');
+
+	var courseData = [];
+
+	chapters.each(function(item) {
+		var chapter = $(this);
+		var chapterTitle = chapter.find('strong').text();
+
+		var videos = chapter.find('.video').children('li');
+
+		var chapterData = {
+			chapterTitle: chapterTitle,
+			videos: []
+		};
+
+		videos.each(function(item) {
+			var video = $(this).find('.J-media-item');
+			var videoTitle = video.text();
+			var videoId = video.attr('href').split('/video/')[1];
+
+			chapterData.videos.push({
+				title: videoTitle,
+				id:  videoId
+			});
+		});
+		courseData.push(chapterData);
+
+	});
+
+	return courseData;
+
+	// [
+	// 	{
+	// 		chapter: '',
+	// 		videos: {
+	// 			title: '',
+	// 			id: ''
+	// 		}
+	// 	}
+	// ]
+
+
+
+}
+
+function printCourseInfo(courseData) {
+	courseData.forEach(function(item) {
+		var chapterTitle = item.chapterTitle;
+
+		console.log(chapterTitle + '\n');
+
+		item.videos.forEach(function(video) {
+			var id = video.id;
+			var title = video.title;
+
+			console.log('【' + id + '】' + title + '\n');
+		});
+	});
+}
 
 http.get(url, function(res) {
 	var html = '';
@@ -7,7 +70,10 @@ http.get(url, function(res) {
 			html += data;
 	});
 	res.on('end', function() {
-		console.log(html);
+
+		var courseData = filterChapters(html);
+
+		printCourseInfo(courseData);
 	});
 }).on('error', function() {
 	console.log('获取课程数据出错!');
