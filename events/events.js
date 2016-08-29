a7f315509b6135a26a8aafe00d5c211b16d1a385
liuweiqin <772863869@qq.com>
2016-08-30 00:30:07 +0800

    移除某个监听事件，使用removeListener()方法

--- events/events.js
+++ events/events.js
@@ -2,12 +2,12 @@ var eventEmitter = require('events').EventEmitter;
 var life = new eventEmitter();
 //给EventEmitter设置最大监听数
 life.setMaxListeners(11);
-
+function listing1(who) {	
+	console.log('1列表' + who );
+}
 
 // on 也可以换成  addEventListener
-life.on('listing', function(who) {
-	console.log('1列表' + who );
-});
+life.on('listing', listing1);
 life.on('listing', function(who) {
 	console.log('2列表' + who);
 });
@@ -40,16 +40,18 @@ life.on('listing', function(who) {
 });
 
 life.on('methods', function(where) {
-	console.log('方法1' + where)
+	console.log('方法1' + where);
 });
 life.on('methods', function(where) {
-	console.log('方法2' + where)
+	console.log('方法2' + where);
 });
+
+life.removeListener('listing', listing1);
 
 var hasListingListener = life.emit('listing', '1');
 var hasMethodListener = life.emit('methods', '2');
 var hasOtherListener = life.emit('other', '2');
 
-console.log(hasListingListener);
-console.log(hasMethodListener);
-console.log(hasOtherListener);
+// console.log(hasListingListener);
+// console.log(hasMethodListener);
+// console.log(hasOtherListener);
