45ec70171a06f4f701aad7573491d953d246a142
liuweiqin <772863869@qq.com>
2016-09-11 12:12:31 +0800

    可读流的 暂停和重启

--- stream/stream_event.js
+++ stream/stream_event.js
@@ -4,17 +4,30 @@ var fs = require('fs');
 //读取文件，创建读取流
 var readStream = fs.createReadStream('stream_copy_logo.js');
  
-//读取流 的事件
+var n = 0;
+
+//可读流 的事件
 readStream
 	.on('data', function(chunk) {
+		n++;
 		console.log('data emits');
 		console.log(Buffer.isBuffer(chunk));
-		console.log(chunk.toString('utf8'));
+		// console.log(chunk.toString('utf8'));
+		// 将可读流暂停
+		readStream.pause();
+		console.log('data pause');
+		//设置3000毫秒的定时器
+		setTimeout(function() {
+			console.log('data pause end');
+			//重启 可读流
+			readStream.resume();
+		}, 3000);
 	})
 	.on('readable', function(){
 		console.log('data readable');
 	})
 	.on('end', function(){
+		console.log(n);
 		console.log('data end');
 	})
 	.on('close', function(){
