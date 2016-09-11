85da081e601b2916ec5bde6c64bf7eb2c05bc028
liuweiqin <772863869@qq.com>
2016-09-11 12:27:49 +0800

    如果读写速度不一样，数据流内部的缓存可能爆仓, 使用pause和resume解决

--- stream/stream_copy_log2.js
+++ stream/stream_copy_log2.js
@@ -2,12 +2,22 @@ var fs = require('fs');
 
 var readStream = fs.createReadStream('haiou.jpg');
 
-var writeStream = fs.createWriteStream('haiou_stream_copy.jpg');
+var writeStream = fs.createWriteStream('haiou_stream_copy_pause.jpg');
 
 readStream.on('data', function(chunk) {
-	writeStream.write(chunk);
+	if (writeStream.write(chunk) === false) {
+		console.log('still cached');
+		readStream.pause();
+	}		
 });
 
 readStream.on('end', function() {
 	writeStream.end();
+});
+
+// drain 耗尽 ，数据已经写入目标
+writeStream.on('drain', function() {
+	console.log('data drain');
+
+	readStream.resume();
 });
